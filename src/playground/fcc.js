3669962febdb38f4e97f93371219aef4294a728e
Sunil <sunilnet4@gmail.com>
2021-10-13 11:51:52 +0530

    wip

---
+++ src/playground/fcc.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const fcc = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default fcc
+
+
+// cd src && mkdir components && cd components && touch Wrapper.js Wrapper.css Screen.js Screen.css ButtonBox.js ButtonBox.css Button.js Button.css
