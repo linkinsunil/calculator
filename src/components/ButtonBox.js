3420f2e2f451e69bdbd5b15c1af50e8791dccb6a
Sunil <sunilnet4@gmail.com>
2021-10-13 11:51:28 +0530

    wip

---
+++ src/components/ButtonBox.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ButtonBox.css'
+
+const ButtonBox = ({children}) => {
+    return (
+        <div className="buttonBox">
+            {children}                        
+        </div>
+    )
+}
+
+export default ButtonBox;
